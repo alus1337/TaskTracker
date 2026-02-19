4ede1c52ebcaa96452aca5c5a600eb3ba4930b1b
alus1337 <167738540+alus1337@users.noreply.github.com>
2026-02-19 02:13:50 -0800

    Add files via upload

--- os_functions.py
+++ os_functions.py
@@ -1,3 +1,29 @@
 import os
+<<<<<<< HEAD
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
+=======
+import sys
+from appdirs import user_data_dir
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def init_data_dir():
+    APP_NAME = "TaskTracker"
+    APP_AUTHOR = "Alus"
+
+    data_dir = user_data_dir(APP_NAME, APP_AUTHOR)
+
+    try:
+        os.makedirs(data_dir, exist_ok=True)
+    except Exception as e:
+        clear()
+        input(f"Error during creating app data directory.\n--\n{e}")
+        sys.exit(1)
+
+    return data_dir
+
+def create_file_path(file_name, ex):
+    # init data dir returns the app data directory 
+    return init_data_dir() + f"/{file_name}.{ex}"
+>>>>>>> 8790970 (added start task)
--- tools.py
+++ tools.py
@@ -1,7 +1,13 @@
+<<<<<<< HEAD
 from appdirs import user_data_dir
 from datetime import datetime
 from enum import Enum
 from os_functions import clear
+=======
+from datetime import datetime
+from enum import Enum
+from os_functions import clear, create_file_path
+>>>>>>> 8790970 (added start task)
 import os
 import sys 
 import json
@@ -9,6 +15,7 @@ import json
 class tools(Enum):
     START_NEW_TASK = '1'
 
+<<<<<<< HEAD
 def init_data_dir():
     APP_NAME = "TaskTracker"
     APP_AUTHOR = "Alus"
@@ -24,6 +31,17 @@ def tools_main():
 
     task = input("Please provide a short task description. Do not include what or why you are doing this task for just simply what needs to be done")
     goal = input("Please provide a short goal that this task contributes to.")
+=======
+def start_new_task():
+    clear()
+
+    task = input("Please provide a short task description. Do not include what or why you are doing this task for just simply what needs to be done\nResponse: ")
+    clear()
+
+    goal = input("Please provide a short goal that this task contributes to.\nResponse: ")
+    clear()
+
+>>>>>>> 8790970 (added start task)
     current_time = datetime.now()
 
     task_object = {
@@ -32,12 +50,25 @@ def tools_main():
         "Start time": current_time.strftime("%Y-%m-%d %H:%M:%S")
     }
 
+<<<<<<< HEAD
     file = f'{init_data_dir()}/{task}.json'
     print(file)
+=======
+    file = create_file_path(task_object["task"], "json")
+>>>>>>> 8790970 (added start task)
     
     with open(file, "w") as f:
         json.dump(task_object, f, indent=4) 
 
     print(task_object)
 
+<<<<<<< HEAD
 tools_main()
+=======
+    
+
+def tools_main(selection='1'):
+    match selection:
+        case tools.START_NEW_TASK.value:
+            start_new_task()
+>>>>>>> 8790970 (added start task)
